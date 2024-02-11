5055babf0496d6b7ef18024040da0a1613c67616
anuskamishra <71863117+anuskamishra@users.noreply.github.com>
2024-02-12 00:55:31 +0530

    Updates

---
+++ precache-manifest.acab9c87538bc7602595ac9f6e55f5bd.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6f414cc62806db264630640082d276",
+    "url": "/Anuska-Mishra-Portfolio/index.html"
+  },
+  {
+    "revision": "59e956b8f4bca856ac6a",
+    "url": "/Anuska-Mishra-Portfolio/static/css/2.fbcfe05c.chunk.css"
+  },
+  {
+    "revision": "dbf92e275769b498fceb",
+    "url": "/Anuska-Mishra-Portfolio/static/css/main.e2bfd845.chunk.css"
+  },
+  {
+    "revision": "59e956b8f4bca856ac6a",
+    "url": "/Anuska-Mishra-Portfolio/static/js/2.71c04dd8.chunk.js"
+  },
+  {
+    "revision": "576ff1be64a9b1c57597b06bbeb80012",
+    "url": "/Anuska-Mishra-Portfolio/static/js/2.71c04dd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbf92e275769b498fceb",
+    "url": "/Anuska-Mishra-Portfolio/static/js/main.4854d8e7.chunk.js"
+  },
+  {
+    "revision": "01aff02a39eaf120077c",
+    "url": "/Anuska-Mishra-Portfolio/static/js/runtime-main.e927346d.js"
+  },
+  {
+    "revision": "2339c4c6f6c15bf5d6529994896d6d7b",
+    "url": "/Anuska-Mishra-Portfolio/static/media/anuskamishraimg.2339c4c6.jpg"
+  },
+  {
+    "revision": "16050e6df873aaa8e2ba505ddc4ab96f",
+    "url": "/Anuska-Mishra-Portfolio/static/media/cp.16050e6d.jpg"
+  },
+  {
+    "revision": "c6a193c5059ff6d4d4dc03c0151a9850",
+    "url": "/Anuska-Mishra-Portfolio/static/media/emojoy.c6a193c5.png"
+  },
+  {
+    "revision": "13c50ffa70c519612db0bcf36851e1aa",
+    "url": "/Anuska-Mishra-Portfolio/static/media/gmail.13c50ffa.png"
+  },
+  {
+    "revision": "326720e2d69cdb1cf3dec1052792b6f1",
+    "url": "/Anuska-Mishra-Portfolio/static/media/instagram.326720e2.png"
+  },
+  {
+    "revision": "5f4fda3ed296f5d562727b27da17d37a",
+    "url": "/Anuska-Mishra-Portfolio/static/media/movie.5f4fda3e.jpg"
+  },
+  {
+    "revision": "2ed0195c937f0e9d857595d3736694d2",
+    "url": "/Anuska-Mishra-Portfolio/static/media/newsmonkey.2ed0195c.webp"
+  },
+  {
+    "revision": "0145fd27b319134079e6ef37de3252f2",
+    "url": "/Anuska-Mishra-Portfolio/static/media/pp.0145fd27.jpg"
+  },
+  {
+    "revision": "596562f76f6401e869d24499c0d60535",
+    "url": "/Anuska-Mishra-Portfolio/static/media/twitter.596562f7.png"
+  },
+  {
+    "revision": "b0502c2c85b7c904c50e6fe9245e0c22",
+    "url": "/Anuska-Mishra-Portfolio/static/media/wesummup.b0502c2c.webp"
+  }
+]);
